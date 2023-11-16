ecf2d462f5ccdbbcd58c8e231562180b688bab6b
Aaron Wang <94033911+philosolog@users.noreply.github.com>
2023-11-16 03:44:53 -0700

    Delete Python directory

--- Python/Codeforces/Educational Codeforces Round 155 (Rated for Div. 2)/B_Chips_on_the_Board.py
+++
@@ -1,6 +0,0 @@
-for t in range(int(input())):
-	n = int(input())
-	a = [int(x) for x in input().split()]
-	b = [int(x) for x in input().split()]
-
-	print(min(min(a)*n + sum(b), min(b)*n + sum(a)))
--- Python/TeamsCode/2023/Summer/4.py
+++
@@ -1,4 +0,0 @@
-for q in range(int(input())):
-	l, r, x = [int(z) for z in input().split()]
-
-	print(x//2-l + 1)
