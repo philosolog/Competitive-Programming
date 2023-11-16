821a45766c48abf2805ca245928f95ca1da61ad3
philosolog <philosolog@gmail.com>
2023-11-16 03:30:59 -0700

    delete 7 files

--- Python/cpython.uz/New folder/A1.py
+++
@@ -1,7 +0,0 @@
-v = 0
-n = int(input())
-
-for i in range(n):
-    v += i + 1
-
-print(v/n)
--- Python/miscellaneous/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-w = [ord(x)-97 for x in input()]
-d = int(input())
-s = int(input())
-nw = [chr(x*d+s%26+97) for x in w]
-
-print(*nw, sep="")
