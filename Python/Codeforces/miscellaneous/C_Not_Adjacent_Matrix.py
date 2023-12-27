6fb4fd89320d2b7d54c34121913f1658a03f3b2d
Aaron Wang <philosolog@gmail.com>
2023-12-27 01:56:24 -0800

    update C_Not_Adjacent_Matrix.py

--- Python/Codeforces/miscellaneous/C_Not_Adjacent_Matrix.py
+++ Python/Codeforces/miscellaneous/C_Not_Adjacent_Matrix.py
@@ -1,13 +1,29 @@
-import numpy as np
-
 for _ in range(int(input())):
 	n = int(input())
+	current_integer = 1
+	diagonal_offset = 0
+	m = [[0]*n for __ in range(n)]
 
 	if n == 1:
 		print(1)
 	elif n == 2:
 		print(-1)
 	else:
-		m = np.triu(np.arange(n^2), -n)
-
-		print(m)
+		for i in range(2*n-1):
+			if diagonal_offset == 0:
+				for j in range(n):
+					m[j][j] = current_integer
+					current_integer += 1
+				
+				diagonal_offset += 1
+			else:
+				for j in range(n-diagonal_offset):
+						m[j][j+diagonal_offset] = current_integer
+						current_integer += 1
+				for j in range(n-diagonal_offset):
+						m[j+diagonal_offset][j] = current_integer
+						current_integer += 1
+				
+				diagonal_offset += 1
+		for i in m:
+			print(*i)
