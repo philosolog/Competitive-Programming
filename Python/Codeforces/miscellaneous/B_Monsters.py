880e3181176dc110b42da0bc827f38f2755a47a4
Aaron Wang <philosolog@gmail.com>
2023-12-30 20:02:07 -0800

    update B_Monsters.py

--- Python/Codeforces/miscellaneous/B_Monsters.py
+++ Python/Codeforces/miscellaneous/B_Monsters.py
@@ -2,4 +2,4 @@ for _ in range(int(input())):
 	n, k = map(int, input().split())
 	m = [int(_)%k or k for _ in input().split()]
 	
-	print(*[x+1 for x in sorted(range(n), key=m.__getitem__, reverse=True)]) # ?: How does this work syntactically?
+	print(*[x+1 for x in sorted(range(n), key=m.__getitem__, reverse=True)]) # ?: How does this work syntactically? (sorted(m, reverse=True)) vs. (sorted(range(n), key=m.__getitem__, reverse=True))?
