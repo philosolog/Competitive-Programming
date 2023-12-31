9056177fd2945add0841c178a3dbc762c8b85d05
Aaron Wang <philosolog@gmail.com>
2023-12-30 20:48:24 -0800

    update A_K_divisible_Sum.py

--- Python/Codeforces/miscellaneous/A_K_divisible_Sum.py
+++ Python/Codeforces/miscellaneous/A_K_divisible_Sum.py
@@ -1,8 +1,14 @@
-import math
+# import math
 
+# for _ in range(int(input())):
+# 	n, k = map(int, input().split())
+	
+# 	print(math.ceil(k*math.ceil(n/k)/n))
+
+# Note: Pigeonhole Principle; floor((n+k-1)/k) = ceil(n/k)
+	
+# In pure Python:
 for _ in range(int(input())):
 	n, k = map(int, input().split())
 	
-	print(math.ceil(k*math.ceil(n/k)/n))
-
-# Note: Pigeonhole Principle
+	print((k*((n+k-1)//k)+n-1)//n)
