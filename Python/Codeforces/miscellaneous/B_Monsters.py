78ad5d279b2c9a7c065a1f975f754ee4622507d5
Aaron Wang <philosolog@gmail.com>
2023-12-30 16:41:23 -0800

    upsolved an old problem

---
+++ Python/Codeforces/miscellaneous/B_Monsters.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+	n, k = map(int, input().split())
+	m = [int(_)%k or k for _ in input().split()]
+	
+	print(*[x+1 for x in sorted(range(n), key=m.__getitem__, reverse=True)]) # ?: How does this work syntactically?
