2571d413b6af80e276be41911a924a6d3deadb5e
Aaron Wang <philosolog@gmail.com>
2024-01-09 10:19:17 -0700

    chore: delete A_Contest_Start.py

--- A_Contest_Start.py
+++ A_Contest_Start.py
@@ -11,12 +11,10 @@ if sys.version_info[0] < 3:
 def main():
 	for _ in range(int(input())):
 		n, x, t = map(int, input().split())
-		d = 0
 
-		for i in range(n):
-			d += min(i, (i*x)//t)
+		print(max(0, n-t//x)*(t//x)+min(n-1, t//x-1)*min(n, t//x)//2)
 
-		print(d)
+	# TODO: Understand the geometry better.
 
 # region fastio
 BUFSIZE = 8192
