073f1887ffac4e178dcb19bf9e26b69405ced0e0
philosolog <philosolog@gmail.com>
2024-01-26 04:25:57 -0700

    update A_Prime_Problem.py

--- Solutions/CBOJ/A_Prime_Problem.py
+++ Solutions/CBOJ/A_Prime_Problem.py
@@ -3,9 +3,27 @@ l = []
 for _ in range(int(input())):
 	a, b = map(int, input().split())
 
-	if len(l) >= b:
-		print(l[max(0, a-2):max(0, b-2)].count(True))
-	else:
-		l.extend([True]*(b-len(l))) # 0 -> 2
+	if len(l) < b:
+		s = len(l)
 
-		 
+		l.extend([True]*(b-s-1)) # 0 -> 2
+		
+		for i, v in enumerate(l):
+			tv = i+2
+
+			if v == True:
+				if tv*2 <= b:
+					fb = s//tv + 2
+					lb = (fb+b-s-1)//tv
+
+					print(fb)
+					for j in range(fb, lb+1):
+						if j*tv <= b:
+							if l[j*tv-2] != False: # ?: Necessary?
+								l[j*tv-2] = False
+						else:
+							break
+				else:
+					break
+	print("-------------------")
+	# print(l[max(0, a-2):b-1].count(True)) # ?: max(0, b-2).. b-1?
