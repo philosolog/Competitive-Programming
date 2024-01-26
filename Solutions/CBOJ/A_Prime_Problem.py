4b75e3451ce1e6fe4b1ad1064fe24801756ea6b8
philosolog <philosolog@gmail.com>
2024-01-26 07:16:06 -0700

    update A_Prime_Problem.py

--- Solutions/CBOJ/A_Prime_Problem.py
+++ Solutions/CBOJ/A_Prime_Problem.py
@@ -16,14 +16,13 @@ for _ in range(int(input())):
 					fb = s//tv + 2 # !: I am dying here. :) The algo. improperly counts past the first extend after 1000?
 					lb = (fb+b-s-1)//tv
 
-					print(fb)
 					for j in range(fb, lb+1):
-						if j*tv <= b:
+						if j*tv <= b-1:
 							if l[j*tv-2] != False: # ?: Necessary?
 								l[j*tv-2] = False
 						else:
 							break
 				else:
 					break
-	print("-------------------")
-	# print(l[max(0, a-2):b-1].count(True)) # ?: max(0, b-2).. b-1?
+	
+	print(l[max(0, a-2):b-1].count(True)) # ?: max(0, b-2).. b-1?
