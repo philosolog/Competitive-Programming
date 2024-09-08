8f7446ce7f68cbf257925d8c03855b9e587bf632
philosolog <philosolog@gmail.com>
2024-09-08 02:45:05 -0700

    feat: Add solution for LeetCode problem 725

---
+++ Solutions/LeetCode/725.split-linked-list-in-parts.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=725 lang=python3
+#
+# [725] Split Linked List in Parts
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        partitions = []
+        length = 0
+        current = head
+
+        while current:
+            length += 1
+            current = current.next
+
+        minimum = length//k
+        remainder = length%k
+        current = head
+
+        for partition in range(k):
+            partitions.append(current)
+            current_size = minimum + (partition < remainder)
+
+            for _ in range(current_size - 1):
+                if current:
+                    current = current.next
+
+            if current:
+                next_part = current.next
+                current.next = None
+                current = next_part
+
+        return partitions
+
+# TODO: Fully understand
+# @lc code=end
+
