ecdfe5ef5467dfdabe6b42ca097920b218436d1d
Aaron Wang <philosolog@gmail.com>
2024-09-08 20:17:26 -0700

    update 2326.spiral-matrix-iv.py

--- Solutions/LeetCode/2326.spiral-matrix-iv.py
+++ Solutions/LeetCode/2326.spiral-matrix-iv.py
@@ -13,9 +13,46 @@
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         matrix = [[-1 for _ in range(n)] for _ in range(m)]
+        mr, mc = 0, 1
+        rl, rr = 0, m-1
+        cl, cr = 0, n-1
+        r, c = 0, 0
 
         while head:
+            for i in range(cl, cr+1):
+                if not head:
+                    break
+                matrix[rl][i] = head.val
+                head = head.next
 
-            head = head.next
+            rl += 1
+
+            for i in range(rl, rr+1):
+                if not head:
+                    break
+                matrix[i][cr] = head.val
+                head = head.next
+
+            cr -= 1
+
+            for i in range(cr, cl-1, -1):
+                if not head:
+                    break
+                matrix[rr][i] = head.val
+                head = head.next
+
+            rr -= 1
+
+            for i in range(rr, rl-1, -1):
+                if not head:
+                    break
+                matrix[i][cl] = head.val
+                head = head.next
+
+            cl += 1
+        
+        return matrix
+
+# TODO: Go over the intuition for implementation
 # @lc code=end
 
