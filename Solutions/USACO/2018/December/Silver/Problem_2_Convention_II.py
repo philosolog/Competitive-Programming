43cb811f0238fdd9180fb5fe2913eb914ff2ea65
philosolog <philosolog@gmail.com>
2024-09-26 04:14:24 -0700

    usaco problem

--- Solutions/USACO/2018/December/Silver/Problem_2_Convention_II.py
+++ Solutions/USACO/2018/December/Silver/Problem_2_Convention_II.py
@@ -1,13 +1,33 @@
 def main():
+	from bisect import bisect_left
+
 	with open("convention2.in", "r") as fin:
 		data = fin.readlines()
-		
-		for line in range(int(data[0])):
-			a, t = map(int, data[line + 1].split())
+		queue = []
 
-			print(a, t)
+		for line in range(int(data[0])):
+			queue.append(list(map(int, data[line + 1].split())))
+
+		queue.sort(key=lambda x: x[0])
+
+		time = 0
+		longest = 0
+
+		while len(queue) > 0:
+			query = queue[0]
+
+			if time <= query[0]:
+				time = query[0] + query[1]
+				queue.pop(0)
+			else:
+				for i in range(bisect_left(queue, [time, 0])-1, -1, -1):
+					longest = max(longest, time - queue[i][0])
+					time += queue[i][1]
+					queue.pop(i)
+
+			
 	
-	result = ""
+	result = str(longest)
 	
 	with open("convention2.out", "w") as fout:
 		fout.write(result)
